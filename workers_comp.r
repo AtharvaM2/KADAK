6c8bbae996d2f4bff432239537898410e0d06391
AnuvaN <anuvanadkar19@gmail.com>
2026-03-10 15:08:41 +1100

    editing stress testing

--- workers_comp.r
+++ workers_comp.r
@@ -414,57 +414,541 @@ lognormal_sev <- glm(
 summary(lognormal_sev)
 
 #######################################
-# Monte Carlo Simulation
+# Monte Carlo Simulation (Independent)
 #######################################
 
 set.seed(123)
 
-n_sim <- 10000   # 10k is usually enough for stable 99% VaR
+n_sim <- 10000
 
-# --- 1. Frequency model inputs ---
+# --- Frequency model parameters ---
 lambda_hat <- predict(nb_freq, type = "response")
 theta_hat  <- nb_freq$theta
 
 n_pol <- length(lambda_hat)
 
-# --- 2. Severity model inputs ---
+# --- Severity model parameters (Gamma approximation) ---
 sev_shape <- 1 / summary(lognormal_sev)$dispersion
 sev_mean  <- mean(worker_data_sev$claim_amount)
 sev_scale <- sev_mean / sev_shape
 
-# --- 3. Monte Carlo Simulation ---
+# Storage objects
 aggregate_loss <- numeric(n_sim)
+freq_sim <- numeric(n_sim)
+sev_sim  <- numeric(n_sim)
 
 for(s in 1:n_sim){
   
-  # Simulate frequency for ALL policies
-  freq_sim <- rnbinom(n_pol,
+  # Simulate frequency for all policies
+  freq_vec <- rnbinom(n_pol,
                       size = theta_hat,
                       mu   = lambda_hat)
   
-  total_claims <- sum(freq_sim)
+  total_claims <- sum(freq_vec)
+  
+  freq_sim[s] <- total_claims
   
   if(total_claims > 0){
     
-    # Simulate all severities in one vectorised draw
     severities <- rgamma(total_claims,
                          shape = sev_shape,
                          scale = sev_scale)
     
-    aggregate_loss[s] <- sum(severities)
+    total_loss <- sum(severities)
+    
+    sev_sim[s] <- mean(severities)
+    
+    aggregate_loss[s] <- total_loss
+    
+  } else {
+    
+    sev_sim[s] <- 0
+    aggregate_loss[s] <- 0
+    
   }
 }
 
-# --- 4. Risk Metrics ---
+# --- Risk Metrics ---
 WC_mean_loss <- mean(aggregate_loss)
 WC_sd_loss   <- sd(aggregate_loss)
-WC_VaR_99    <- quantile(aggregate_loss, 0.99)
-WC_TVaR_99   <- mean(aggregate_loss[aggregate_loss > WC_VaR_99])
+
+WC_VaR_99  <- quantile(aggregate_loss, 0.99)
+WC_TVaR_99 <- mean(aggregate_loss[aggregate_loss > WC_VaR_99])
 
 WC_mean_loss
 WC_sd_loss
 WC_VaR_99
 WC_TVaR_99
+
+#######################################
+# Stress testing
+#######################################
+##stress levels 
+#frequency & severity 
+freq_stress_levels <- c(1.25, 1.5, 1.75, 2.0)   
+sev_stress_levels  <- c(1.25, 1.5, 1.75, 2.0)        
+length_stress_levels <- c(1.25, 1.5, 1.75, 2.0)   
+
+#operational risk 
+exposure_levels <- c(1.25, 1.5, 1.75, 2.0)
+hours_levels <- c(1.5, 2.0, 2.5)         
+supervision_levels <- c(1.4, 1.2, 1, 0.8, 0.6, 0.4)
+experience_levels <- c(1.4, 1.2, 1, 0.8, 0.6, 0.4)
+
+#safety and training 
+training_stress_levels <- c(0.5, 0.75, 1, 1.25, 1.5, 1.75, 2.0)
+gravity_stress_levels <- c(0.5, 0.75, 1, 1.25, 1.5, 1.75, 2.0)
+
+#psychological impact
+psych_stress_levels <- c(0.8, 1, 1.2, 1.4, 1.6, 1.8, 2.0)
+correlation_levels <- c(0.1, 0.3, 0.5, 0.7, 0.9, 1)
+
+#occupation 
+occ_levels <- c(0.5, 0.75, 1, 1.25, 1.5, 1.75, 2.0)
+worker_levels <- c(0.6, 0.8, 1, 1.2, 1.4, 1.6, 1.8, 2.0)
+
+#financial / economic
+inflation_levels <- c(1, 1.05, 1.10, 1.15, 1.20, 1.30, 1.35, 1.4, 1.45, 1.5)
+salary_levels <- c(1, 1.1, 1.2, 1.3, 1.4, 1.5)  
+
+
+#######################################
+# Frequency Stress Test
+#######################################
+
+freq_stress_levels <- c(1.25,1.5,1.75,2)
+
+freq_results <- data.frame()
+
+for(f in freq_stress_levels){
+  
+  freq_temp <- round(freq_sim * f)
+  
+  loss_temp <- sapply(freq_temp, function(n){
+    
+    if(n == 0) return(0)
+    
+    sum(rgamma(n,
+               shape = sev_shape,
+               scale = sev_scale))
+    
+  })
+  
+  VaR <- quantile(loss_temp,0.99)
+  
+  freq_results <- rbind(freq_results,
+                        data.frame(
+                          stress_factor = f,
+                          mean_loss = mean(loss_temp),
+                          VaR_99 = VaR,
+                          TVaR_99 = mean(loss_temp[loss_temp > VaR])
+                        ))
+}
+
+freq_results
+
+#######################################
+# Severity Stress Test
+#######################################
+
+sev_stress_levels <- c(1.25,1.5,1.75,2)
+
+sev_results <- data.frame()
+
+for(s in sev_stress_levels){
+  
+  sev_scale_stress <- sev_scale * s
+  
+  loss_temp <- sapply(freq_sim, function(n){
+    
+    if(n == 0) return(0)
+    
+    sum(rgamma(n,
+               shape = sev_shape,
+               scale = sev_scale_stress))
+    
+  })
+  
+  VaR <- quantile(loss_temp,0.99)
+  
+  sev_results <- rbind(sev_results,
+                       data.frame(
+                         stress_factor = s,
+                         mean_loss = mean(loss_temp),
+                         VaR_99 = VaR,
+                         TVaR_99 = mean(loss_temp[loss_temp > VaR])
+                       ))
+}
+
+sev_results
+
+#######################################
+# Claim Length Stress Test
+#######################################
+
+claim_length <- na.omit(worker_data_sev$claim_length)
+
+daily_wage <- mean(worker_data_sev$claim_amount) / mean(claim_length)
+
+length_stress_levels <- c(1.25,1.5,1.75,2)
+
+length_results <- data.frame()
+
+for(l in length_stress_levels){
+  
+  length_stress <- claim_length * l
+  severity_temp <- length_stress * daily_wage
+  
+  loss_temp <- sapply(freq_sim, function(n){
+    
+    if(n == 0) return(0)
+    
+    sum(sample(severity_temp, n, replace = TRUE))
+    
+  })
+  
+  VaR <- quantile(loss_temp,0.99)
+  
+  length_results <- rbind(length_results,
+                          data.frame(
+                            stress_factor = l,
+                            mean_loss = mean(loss_temp),
+                            VaR_99 = VaR,
+                            TVaR_99 = mean(loss_temp[loss_temp > VaR])
+                          ))
+}
+
+length_results
+
+#######################################
+# Combine Stress Results
+#######################################
+
+freq_results$test_type   <- "Frequency"
+sev_results$test_type    <- "Severity"
+length_results$test_type <- "Claim Length"
+
+stress_results <- rbind(freq_results,
+                        sev_results,
+                        length_results)
+
+stress_results
+
+#######################################
+# Exposure Stress Test
+#######################################
+
+exposure_results <- data.frame()
+
+for(e in exposure_levels){
+  
+  freq_temp <- round(freq_sim * e)
+  
+  loss_temp <- sapply(freq_temp, function(n){
+    
+    if(n == 0) return(0)
+    
+    sum(rgamma(n,
+               shape = sev_shape,
+               scale = sev_scale))
+    
+  })
+  
+  VaR <- quantile(loss_temp,0.99)
+  
+  exposure_results <- rbind(exposure_results,
+                            data.frame(
+                              stress_factor = e,
+                              mean_loss = mean(loss_temp),
+                              VaR_99 = VaR,
+                              TVaR_99 = mean(loss_temp[loss_temp > VaR])
+                            ))
+}
+exposure_results
+#######################################
+# Hours Worked Stress Test
+#######################################
+
+hours_results <- data.frame()
+
+for(h in hours_levels){
+  
+  freq_temp <- round(freq_sim * h)
+  
+  loss_temp <- sapply(freq_temp, function(n){
+    
+    if(n == 0) return(0)
+    
+    sum(rgamma(n,
+               shape = sev_shape,
+               scale = sev_scale))
+    
+  })
+  
+  VaR <- quantile(loss_temp,0.99)
+  
+  hours_results <- rbind(hours_results,
+                         data.frame(
+                           stress_factor = h,
+                           mean_loss = mean(loss_temp),
+                           VaR_99 = VaR,
+                           TVaR_99 = mean(loss_temp[loss_temp > VaR])
+                         ))
+}
+hours_results
+#######################################
+# Supervision Stress Test
+#######################################
+
+supervision_results <- data.frame()
+
+for(s in supervision_levels){
+  
+  freq_temp <- round(freq_sim * (1 + (1 - s)))
+  
+  loss_temp <- sapply(freq_temp, function(n){
+    
+    if(n == 0) return(0)
+    
+    sum(rgamma(n,
+               shape = sev_shape,
+               scale = sev_scale))
+    
+  })
+  
+  VaR <- quantile(loss_temp,0.99)
+  
+  supervision_results <- rbind(supervision_results,
+                               data.frame(
+                                 stress_factor = s,
+                                 mean_loss = mean(loss_temp),
+                                 VaR_99 = VaR,
+                                 TVaR_99 = mean(loss_temp[loss_temp > VaR])
+                               ))
+}
+supervision_results
+#######################################
+# Experience Stress Test
+#######################################
+
+experience_results <- data.frame()
+
+for(x in experience_levels){
+  
+  freq_temp <- round(freq_sim * (1 + (1 - x)))
+  
+  loss_temp <- sapply(freq_temp, function(n){
+    
+    if(n == 0) return(0)
+    
+    sum(rgamma(n,
+               shape = sev_shape,
+               scale = sev_scale))
+    
+  })
+  
+  VaR <- quantile(loss_temp,0.99)
+  
+  experience_results <- rbind(experience_results,
+                              data.frame(
+                                stress_factor = x,
+                                mean_loss = mean(loss_temp),
+                                VaR_99 = VaR,
+                                TVaR_99 = mean(loss_temp[loss_temp > VaR])
+                              ))
+}
+experience_results
+#######################################
+# Training / PPE Stress Test
+#######################################
+
+training_results <- data.frame()
+
+for(t in training_stress_levels){
+  
+  sev_scale_stress <- sev_scale * t
+  
+  loss_temp <- sapply(freq_sim, function(n){
+    
+    if(n == 0) return(0)
+    
+    sum(rgamma(n,
+               shape = sev_shape,
+               scale = sev_scale_stress))
+    
+  })
+  
+  VaR <- quantile(loss_temp,0.99)
+  
+  training_results <- rbind(training_results,
+                            data.frame(
+                              stress_factor = t,
+                              mean_loss = mean(loss_temp),
+                              VaR_99 = VaR,
+                              TVaR_99 = mean(loss_temp[loss_temp > VaR])
+                            ))
+}
+training_results
+#######################################
+# Gravity / Hazard Stress Test
+#######################################
+
+gravity_results <- data.frame()
+
+for(g in gravity_stress_levels){
+  
+  freq_temp <- round(freq_sim * g)
+  
+  sev_scale_stress <- sev_scale * g
+  
+  loss_temp <- sapply(freq_temp, function(n){
+    
+    if(n == 0) return(0)
+    
+    sum(rgamma(n,
+               shape = sev_shape,
+               scale = sev_scale_stress))
+    
+  })
+  
+  VaR <- quantile(loss_temp,0.99)
+  
+  gravity_results <- rbind(gravity_results,
+                           data.frame(
+                             stress_factor = g,
+                             mean_loss = mean(loss_temp),
+                             VaR_99 = VaR,
+                             TVaR_99 = mean(loss_temp[loss_temp > VaR])
+                           ))
+}
+gravity_results
+#######################################
+# Psychological Stress Test
+#######################################
+
+psych_results <- data.frame()
+
+for(s in psych_stress_levels){
+  
+  freq_temp <- round(freq_sim * s)
+  
+  sev_scale_stress <- sev_scale * (1 + (s - 1)*0.5)
+  
+  loss_temp <- sapply(freq_temp, function(n){
+    
+    if(n == 0) return(0)
+    
+    sum(rgamma(n,
+               shape = sev_shape,
+               scale = sev_scale_stress))
+    
+  })
+  
+  VaR <- quantile(loss_temp,0.99)
+  
+  psych_results <- rbind(psych_results,
+                         data.frame(
+                           stress_factor = s,
+                           mean_loss = mean(loss_temp),
+                           VaR_99 = VaR,
+                           TVaR_99 = mean(loss_temp[loss_temp > VaR])
+                         ))
+}
+psych_results
+#######################################
+# Claims Inflation Stress Test
+#######################################
+
+inflation_results <- data.frame()
+
+for(i in inflation_levels){
+  
+  sev_scale_stress <- sev_scale * i
+  
+  loss_temp <- sapply(freq_sim, function(n){
+    
+    if(n == 0) return(0)
+    
+    sum(rgamma(n,
+               shape = sev_shape,
+               scale = sev_scale_stress))
+    
+  })
+  
+  VaR <- quantile(loss_temp,0.99)
+  
+  inflation_results <- rbind(inflation_results,
+                             data.frame(
+                               stress_factor = i,
+                               mean_loss = mean(loss_temp),
+                               VaR_99 = VaR,
+                               TVaR_99 = mean(loss_temp[loss_temp > VaR])
+                             ))
+}
+inflation_results
+#######################################
+# Salary Linked Claims Stress Test
+#######################################
+
+salary_results <- data.frame()
+
+avg_salary <- mean(worker_data_sev$base_salary, na.rm = TRUE)
+
+for(s in salary_levels){
+  
+  salary_multiplier <- s
+  
+  sev_scale_stress <- sev_scale * salary_multiplier
+  
+  loss_temp <- sapply(freq_sim, function(n){
+    
+    if(n == 0) return(0)
+    
+    sum(rgamma(n,
+               shape = sev_shape,
+               scale = sev_scale_stress))
+    
+  })
+  
+  VaR <- quantile(loss_temp,0.99)
+  
+  salary_results <- rbind(salary_results,
+                          data.frame(
+                            stress_factor = s,
+                            mean_loss = mean(loss_temp),
+                            VaR_99 = VaR,
+                            TVaR_99 = mean(loss_temp[loss_temp > VaR])
+                          ))
+}
+salary_results
+
+#######################################
+# Combined Results
+#######################################
+
+stress_results_final <- rbind(
+  transform(exposure_results, test="Exposure"),
+  transform(hours_results, test="Hours"),
+  transform(supervision_results, test="Supervision"),
+  transform(experience_results, test="Experience"),
+  transform(training_results, test="Training/PPE"),
+  transform(gravity_results, test="Gravity Hazard"),
+  transform(psych_results, test="Psych Stress"),
+  transform(inflation_results, test="Claims Inflation"),
+  transform(salary_results, test="Salary Linked")
+)
+
+ggplot(stress_results_final,
+       aes(x = stress_factor,
+           y = VaR_99,
+           color = test)) +
+  geom_line() +
+  geom_point() +
+  labs(title = "Workers Compensation Stress Testing",
+       x = "Stress Multiplier",
+       y = "VaR 99%") +
+  theme_minimal()
+
 
 #######################################
 # Tail Dependence
